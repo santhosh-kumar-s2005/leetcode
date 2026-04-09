3f1ab21255df511f0c3b33cac06a2822262e67df
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-10 01:45:36 +0530

    Time: 1 ms (100%), Space: 47.1 MB (7.36%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -2,7 +2,7 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> arr=new ArrayList<>();
         for(int i=1;i<=n;i++){
-            if((i%3==0 && i%5==0)){
+            if((i%15==0)){
                 arr.add("FizzBuzz");
             }
             else  if((i%3==0 )){
