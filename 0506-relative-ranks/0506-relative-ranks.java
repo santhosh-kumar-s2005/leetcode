ba4971cef36c7496f0adfb0a380face6870afb84
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-14 01:37:37 +0530

    Time: 17 ms (17.01%), Space: 47.3 MB (93.57%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        PriorityQueue<Integer> heap=new PriorityQueue<>((a,b)->b-a);
+        for(int i:score){
+            heap.add(i);
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int rank=1;
+        while(!heap.isEmpty()){
+            map.put(heap.poll(),rank++);
+        }
+        String[] res=new String[score.length];
+        for(int i=0;i<score.length;i++){
+            if(map.get(score[i])==1){
+                res[i]="Gold Medal";
+            }
+            else if(map.get(score[i])==2){
+                res[i]="Silver Medal";
+            }
+            else if(map.get(score[i])==3){
+                res[i]="Bronze Medal";
+            }
+            else {
+                res[i]=""+map.get(score[i]);
+            }
+        }
+        return res;
+
+        
+    }
+}
