d41a88792d713558b94961e231c081962a4e7acd
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-13 17:26:17 +0530

    Time: 2 ms (95.43%), Space: 43.5 MB (52.81%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,35 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int calPoints(String[] operations) {
-       ArrayList<Integer> arr=new ArrayList<>();
-       int ans=0;
-        for(int i=0;i<operations.length;i++){
-            
-            if(operations[i].equals("C")){
-                if(arr.size()>=1){
-                    ans-=arr.remove(arr.size()-1);
-                }
-            }
-            else if(operations[i].equals("D")){
-                if(arr.size()>=1){
-                arr.add(arr.get(arr.size()-1)*2);
-                ans+=arr.get(arr.size()-1);
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int ans = 0;
 
-            }
-            else if(operations[i].equals("+")){
-                if(arr.size()>=2){
-                    arr.add(arr.get(arr.size()-1)+arr.get(arr.size()-2));
-                    ans+=arr.get(arr.size()-1);
-                }
+        for (String op : operations) {
 
+            if (op.equals("C")) {
+                ans -= stack.pop();
+            } 
+            else if (op.equals("D")) {
+                int val = stack.peek() * 2;
+                stack.push(val);
+                ans += val;
+            } 
+            else if (op.equals("+")) {
+                int top = stack.pop();
+                int newVal = top + stack.peek();
+                stack.push(top);      // restore
+                stack.push(newVal);
+                ans += newVal;
+            } 
+            else {
+                int val = Integer.parseInt(op);
+                stack.push(val);
+                ans += val;
             }
-            else{
-                ans+=Integer.parseInt(operations[i]);
-                arr.add(Integer.parseInt(operations[i]));
-            }
-            System.out.println(ans);
         }
+
         return ans;
-        
     }
 }
