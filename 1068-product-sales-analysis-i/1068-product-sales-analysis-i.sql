783d93819c37fc016d927bb62a3661a12de14393
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-14 13:38:35 +0530

    Time: 1219 ms (51.41%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,1 +1,7 @@
-select p.product_name,s.year,s.price from Sales s  join Product p on s.product_id= p.product_id;
+-- select p.product_name,s.year,s.price from Sales s  join Product p on s.product_id= p.product_id;
+select p.product_name,
+s.year,
+s.price
+FROM product p
+JOIN sales s
+ON p.product_id = s.product_id
