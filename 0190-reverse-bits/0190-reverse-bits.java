9a79b0ebddff38e370e896d5f07a1fc28801eac0
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-16 11:45:57 +0530

    Time: 1 ms (62.62%), Space: 42.3 MB (78.57%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,35 @@
 class Solution {
     public int reverseBits(int n) {
-        int res=0;
-        int c=31;
-        while(n!=0){
-            int t=n&1;
-            n=n>>1;
-            res=res|(t<<c);
-            c--;
+        int i=31;
+
+        while(i>=16){
+            int l = (n >> i) & 1;
+            int r = (n >> (31 - i)) & 1;
+            // int l=n&(1<<i);
+            // int r=n&(1<<(31-i));
+            n=n&~(1<<i);
+            n=n|(r<<i);
+            n=n&~(1<<(31-i));
+            n=n|(l<<(31-i));
+            i--;
+            
         }
-        return res;
+        return n;
         
     }
 }
+
+// class Solution {
+//     public int reverseBits(int n) {
+//         int res=0;
+//         int c=31;
+//         while(n!=0){
+//             int t=n&1;
+//             n=n>>1;
+//             res=res|(t<<c);
+//             c--;
+//         }
+//         return res;
+        
+//     }
+// }
