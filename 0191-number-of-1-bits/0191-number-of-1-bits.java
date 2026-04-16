31b1fba7bb159f6dbffe51ad076b6d94a12d30f2
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-16 12:45:43 +0530

    Time: 1 ms (32.39%), Space: 42.2 MB (92.65%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -6,7 +6,7 @@ class Solution {
         int count =0;
         while(n!=0){
             if((n&1)==1) count++;
-            n=n>>1;
+            n=n>>>1;
             
         }
         return count;
