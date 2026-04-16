3abd419c812975fd7f0d87cb35c35b611843cc13
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-16 10:35:24 +0530

    Time: 7 ms (9.02%), Space: 45.1 MB (9.01%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,32 +1,62 @@
+//class Solution {
+//    public String addBinary(String a, String b) {
+        // int n1=Integer.parseInt(a,2);
+        // int n2=Integer.parseInt(b,2);
+        // return Integer.toString(n1+n2,2);
+
+        // int n1 = a.length(), n2 = b.length();
+
+        // // Make lengths equal
+        // if (n1 > n2) {
+        //     b = "0".repeat(n1 - n2) + b;
+        // } else if (n2 > n1) {
+        //     a = "0".repeat(n2 - n1) + a;
+        // }
+
+        // int n = Math.max(n1, n2);
+        // char arr[] = new char[n];
+
+        // int carry = 0;
+
+        // // Traverse from right to left
+        // for (int i = n - 1; i >= 0; i--) {
+        //     int sum = (a.charAt(i) - '0') + (b.charAt(i) - '0') + carry;
+
+        //     arr[i] = (char) ((sum % 2) + '0'); // store bit
+        //     carry = sum / 2;                  // update carry
+        // }
+
+        // // If carry remains
+        // if (carry == 1) {
+        //     return "1" + new String(arr);
+        // }
+
+        // return new String(arr);
+   // }
+//}
+// import java.math.BigInteger;
+
+// class Solution {
+//     public String addBinary(String a, String b) {
+//         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
+//     }
+// }
+import java.math.BigInteger;
+
 class Solution {
     public String addBinary(String a, String b) {
-        int n1 = a.length(), n2 = b.length();
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger zero = new BigInteger("0", 2);
+        BigInteger carry, answer;
 
-        // Make lengths equal
-        if (n1 > n2) {
-            b = "0".repeat(n1 - n2) + b;
-        } else if (n2 > n1) {
-            a = "0".repeat(n2 - n1) + a;
+        while (y.compareTo(zero) != 0) {
+            answer = x.xor(y);
+            carry = x.and(y).shiftLeft(1);
+            x = answer;
+            y = carry;
         }
 
-        int n = Math.max(n1, n2);
-        char arr[] = new char[n];
-
-        int carry = 0;
-
-        // Traverse from right to left
-        for (int i = n - 1; i >= 0; i--) {
-            int sum = (a.charAt(i) - '0') + (b.charAt(i) - '0') + carry;
-
-            arr[i] = (char) ((sum % 2) + '0'); // store bit
-            carry = sum / 2;                  // update carry
-        }
-
-        // If carry remains
-        if (carry == 1) {
-            return "1" + new String(arr);
-        }
-
-        return new String(arr);
+        return x.toString(2);
     }
 }
