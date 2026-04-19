0c40eeac543dd6f36dd6456a2d9413f599795c63
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-19 23:15:11 +0530

    Time: 1 ms (78.75%), Space: 42.3 MB (70.2%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set=new HashSet<>();
+        while(n!=1){
+            if(!set.add(n)){
+                return false;
+            }
+            n=call(n); 
+          //  System.out.println(n);
+        }
+        return true;
+    }
+    public static int call(int n){
+        return (n==0)?0:((n%10)*(n%10))+call(n/10);
+    }
+}
