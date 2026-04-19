23fb7e10814fb224f61f9456fd464908fc761751
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-20 04:08:36 +0530

    Time: 1 ms (99.45%), Space: 42.4 MB (74.22%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isUgly(int n) {
         if (n <= 0) return false;
 
-        int[] factors = {5,3,2};
+        int[] factors = {3,5,2};
 
         for (int f : factors) {
             while (n % f == 0) {
