b17f36296b772b86f16c0ab898e87aabad51197c
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-21 00:47:31 +0530

    Time: 1 ms (99.86%), Space: 43.4 MB (24.06%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,14 +1,24 @@
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int sbyte=0;
+//         int tbyte=0;
+//         for(char i:s.toCharArray()){
+//             sbyte+=i;
+//         }
+//          for(char i:t.toCharArray()){
+//             tbyte+=i;
+//         }
+//         tbyte=tbyte-sbyte;
+//         return ((char)tbyte);
+//     }
+// }
 class Solution {
     public char findTheDifference(String s, String t) {
-        int sbyte=0;
-        int tbyte=0;
-        for(char i:s.toCharArray()){
-            sbyte+=i;
-        }
-         for(char i:t.toCharArray()){
-            tbyte+=i;
-        }
-        tbyte=tbyte-sbyte;
-        return ((char)tbyte);
+        char res = 0;
+
+        for(char c : s.toCharArray()) res ^= c;
+        for(char c : t.toCharArray()) res ^= c;
+
+        return res;
     }
 }
