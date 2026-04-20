82e8a763297dc0bacaa5343d85008737119fec81
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-20 09:20:23 +0530

    Time: 8 ms (91.83%), Space: 46.2 MB (30.23%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,11 +1,6 @@
-class Solution {
+public class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<=0){
-            return false;
-        }
-        while(n%3==0){
-            n=n/3;
-        }
-        return n==1;
+        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
+        return n > 0 && maxPowerOf3 % n == 0;
     }
 }
