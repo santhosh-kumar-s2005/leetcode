63eba3039e09ff460609ccf5e857a18216fa4e15
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-22 02:24:05 +0530

    Time: 5 ms (89.61%), Space: 49.6 MB (19.07%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,30 +1,49 @@
+// class Solution {
+//     public List<String> readBinaryWatch(int turnedOn) {
+//         List<String> res =new ArrayList<>();
+//         if(turnedOn==0){
+//             res.add("0:00");
+//             return res;
+//         }
+//         int[] dp = new int[1024];
+        
+//         for(int i = 1; i <= 1023; i++){
+//             dp[i] = dp[i & (i - 1)] + 1;
+//             if(dp[i]==turnedOn){
+//                 int n=i;
+//                 int hour=((n&960)>>6);
+//                 int minute=((n&63));
+//                 if(hour>11 || minute>59){
+//                     continue;
+//                 }
+//                 else{
+//                     res.add(""+hour+":"+((minute<10)?"0"+minute:""+minute));
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
-        List<String> res =new ArrayList<>();
-        if(turnedOn==0){
-            res.add("0:00");
-            return res;
-        }
-        int[] dp = new int[1024];
+        List<String> result = new ArrayList<>();
         
-        for(int i = 1; i <= 1023; i++){
-            dp[i] = dp[i & (i - 1)] + 1;
-            if(dp[i]==turnedOn){
-                int n=i;
-                int hour=((n&960)>>6);
-                int minute=((n&63));
-                if(hour>11 || minute>59){
-                    continue;
-                }
-                else{
-                    res.add(""+hour+":"+((minute<10)?"0"+minute:""+minute));
+        for (int hour = 0; hour < 12; hour++) {
+            
+            for (int minute = 0; minute < 60; minute++) {
+                
+                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+                
+                if (totalBits == turnedOn) {
+                    
+                    String time = hour + ":" + 
+                                  (minute < 10 ? "0" + minute : minute);
+                    
+                    result.add(time);
                 }
             }
         }
-        return res;
-    
-
-
-
+        
+        return result;
     }
 }
