99e8c18fb20866ca59fbf5c94cefdf439824caf1
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-22 18:41:41 +0530

    Time: 0 ms (100%), Space: 45.8 MB (56.47%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] res) {
+        int n=res.length;
+        boolean odd=false;
+        if(n%2==1){
+            odd=true;
+        }
+        for(int i=0;i<n;i++){
+            int p1=0;
+            int p2=n-1;
+            while(p1<p2){
+                int t1=res[i][p1];
+                int t2=res[i][p2];
+                res[i][p1]=(t2==0)?1:0;
+                res[i][p2]=(t1==0)?1:0;
+                p1++;
+                p2--;
+            }
+            if(odd){
+                res[i][n/2]=(res[i][n/2]==0)?1:0;
+            }
+        }
+        return res;
+    }
+
+}
