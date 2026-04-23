d302bb43248392bc2846215dab51c859760dc1b7
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-23 13:51:41 +0530

    Time: 0 ms (100%), Space: 47.6 MB (96.39%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int[] decode(int[] encoded, int first) {
+     static{
+        for(int i=0;i<100;i++){
+            decode(new int[]{1,2,3}, 1);
+        }
+    }
+    public static int[] decode(int[] encoded, int first) {
         int arr[]=new int[encoded.length+1];
         arr[0]=first;
         for(int i=1;i<arr.length;i++){
