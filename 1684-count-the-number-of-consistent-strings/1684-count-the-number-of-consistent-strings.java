dac86846309e595f3537df4b9fee5d0fce0d45a3
santhosh-kumar-s2005 <23it1218@stjosephstechnology.ac.in>
2026-04-23 13:30:48 +0530

    Time: 6 ms (93.06%), Space: 47.9 MB (23.41%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -1,24 +1,50 @@
+// class Solution {
+//     public int countConsistentStrings(String allowed, String[] words) {
+//         HashSet<Character> set=new HashSet<>();
+//         for(char i:allowed.toCharArray()){
+//             set.add(i);
+//         }
+//         int count=0;
+//         for(String i:words){
+//             boolean found=true;
+//             for(char j:i.toCharArray()){
+//                 if(!set.contains(j)){
+//                     found=false;
+//                     break;
+
+//                 }
+//             }
+//             if(found){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+
+// }
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        HashSet<Character> set=new HashSet<>();
-        for(char i:allowed.toCharArray()){
-            set.add(i);
-        }
-        int count=0;
-        for(String i:words){
-            boolean found=true;
-            for(char j:i.toCharArray()){
-                if(!set.contains(j)){
-                    found=false;
-                    break;
+        int mask = 0;
 
+        for (char ch : allowed.toCharArray()) {
+            mask |= (1 << (ch - 'a'));
+        }
+
+        int count = 0;
+
+        for (String word : words) {
+            boolean ok = true;
+
+            for (char ch : word.toCharArray()) {
+                if ((mask & (1 << (ch - 'a'))) == 0) {
+                    ok = false;
+                    break;
                 }
             }
-            if(found){
-                count++;
-            }
+
+            if (ok) count++;
         }
+
         return count;
     }
-
 }
