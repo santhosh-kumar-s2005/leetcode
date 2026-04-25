81bcc35b96c1efbaeae7f6117d8d5dd1036568e0
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-04-26 02:51:30 +0530

    Time: 14 ms (49.26%), Space: 42.1 MB (36.78%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -7,17 +7,16 @@ public class Solution extends VersionControl {
         int left=1;
         int right=n;
         int last=n;
-        while(left<=right){
+        while(left<right){
             int mid=left+((right-left)/2);
             if(isBadVersion(mid)){
-                last=mid;
-                right=mid-1;
+                right=mid;
             }
             else{
                 left=mid+1;
             }
         }
-        return last;
+        return right;
 
         
     }
