c70c0c174ab7e9f0ec25359dc82052e88c53ccde
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-04-26 18:34:12 +0530

    Time: 1 ms (99.34%), Space: 43.6 MB (91.8%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
@@ -1,36 +1,14 @@
-// class Solution {
-//     public int minOperations(List<Integer> nums, int k) {
-//         HashSet<Integer> set=new HashSet<>();
-//         int op=0;
-//         for(int i=nums.size()-1;i>=0;i--){
-//             op++;
-//             if(nums.get(i)>k) continue;
-//             set.add(nums.get(i));
-//             if(set.size()==k){
-//                 break;
-//             }
-//         }
-//         return op;
- 
-
- 
-//     }
-// }    
 class Solution {
     public int minOperations(List<Integer> nums, int k) {
-        int countK = 0, size = nums.size();
-        boolean[] seen = new boolean[k + 1];
-
-        for (int i = size - 1; i >= 0; i--) {
-            int val = nums.get(i);
-
-            if (val <= k && !seen[val]) {
-                seen[val] = true;
-                countK++;
+        long flag = 0; // Use this long variable to mark numbers
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums.get(i) <= k) {
+                flag |= 1L << (nums.get(i) - 1); // Mark if the number is less than or equal to the required k
             }
-            if (countK == k) return size - i;
+            if (flag == (1L << k) - 1) { // If all numbers from 1 to k are marked
+                return nums.size() - i;
+            }
         }
-
         return -1;
     }
 }
