eca04be3dcd0013fbd0dfc4cd0562fcbf50515c1
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-04-26 18:31:27 +0530

    Time: 1 ms (99.34%), Space: 43.7 MB (71.8%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
@@ -1,18 +1,36 @@
-class Solution {
-    public int minOperations(List<Integer> nums, int k) {
-        HashSet<Integer> set=new HashSet<>();
-        int op=0;
-        for(int i=nums.size()-1;i>=0;i--){
-            op++;
-            if(nums.get(i)>k) continue;
-            set.add(nums.get(i));
-            if(set.size()==k){
-                break;
-            }
-        }
-        return op;
+// class Solution {
+//     public int minOperations(List<Integer> nums, int k) {
+//         HashSet<Integer> set=new HashSet<>();
+//         int op=0;
+//         for(int i=nums.size()-1;i>=0;i--){
+//             op++;
+//             if(nums.get(i)>k) continue;
+//             set.add(nums.get(i));
+//             if(set.size()==k){
+//                 break;
+//             }
+//         }
+//         return op;
  
 
  
+//     }
+// }    
+class Solution {
+    public int minOperations(List<Integer> nums, int k) {
+        int countK = 0, size = nums.size();
+        boolean[] seen = new boolean[k + 1];
+
+        for (int i = size - 1; i >= 0; i--) {
+            int val = nums.get(i);
+
+            if (val <= k && !seen[val]) {
+                seen[val] = true;
+                countK++;
+            }
+            if (countK >= k) return size - i;
+        }
+
+        return -1;
     }
-}    
+}
