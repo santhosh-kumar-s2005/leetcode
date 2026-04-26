fe881f0883bb8c1f472036bef26f6800d0ac7340
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-04-26 19:30:33 +0530

    Time: 1 ms (77.44%), Space: 45.5 MB (61.8%) - LeetHub

--- 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.java
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.java
@@ -1,11 +1,9 @@
 class Solution {
-    public boolean hasTrailingZeros(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-            if((nums[i]|nums[j])%2==0){
-                return true;
-            }
-            }
+    public boolean hasTrailingZeros(int[] nums) 
+    {
+        int evenCnt = 0;
+        for (int n : nums) {
+            if (n % 2 == 0 && ++evenCnt >= 2) return true;
         }
         return false;
     }
