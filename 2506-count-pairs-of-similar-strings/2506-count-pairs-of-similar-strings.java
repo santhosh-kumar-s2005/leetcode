93210b4eafd0ce77827f775e0ac0ca487d7b03ce
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-04-26 12:53:03 +0530

    Time: 4 ms (94.09%), Space: 46.2 MB (86.92%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
@@ -1,30 +1,52 @@
+// class Solution {
+//     public int similarPairs(String[] words) {
+//         int arr[]=new int[words.length];
+//         int n=arr.length;
+//         for(int i=0;i<n;i++){
+//             int l=words[i].length();
+//             int res=0;
+//             for(int j=0;j<l;j++){
+//                 res|=1<<(words[i].charAt(j)-'a');
+//             }
+//             arr[i]=res;
+//         }
+//         int count=0;
+//         // HashSet<Integer> set=new HashSet<>();
+//         // for(int i:arr){
+//         //     if(!set.add(i)){
+//         //         count++;
+//         //     }
+//         // }
+//         // return count+1;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[i]==arr[j]){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
 class Solution {
     public int similarPairs(String[] words) {
-        int arr[]=new int[words.length];
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            int l=words[i].length();
-            int res=0;
-            for(int j=0;j<l;j++){
-                res|=1<<(words[i].charAt(j)-'a');
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (String word : words) {
+
+            int mask = 0;
+
+            for (char ch : word.toCharArray()) {
+                mask |= 1 << (ch - 'a');
             }
-            arr[i]=res;
+
+            count += map.getOrDefault(mask, 0);
+
+            map.put(mask, map.getOrDefault(mask, 0) + 1);
         }
-        int count=0;
-        // HashSet<Integer> set=new HashSet<>();
-        // for(int i:arr){
-        //     if(!set.add(i)){
-        //         count++;
-        //     }
-        // }
-        // return count+1;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                }
-            }
-        }
+
         return count;
     }
 }
