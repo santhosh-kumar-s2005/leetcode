b77f0830a68032ef705e3bde305bb145511cee7a
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-05-08 02:11:39 +0530

    Time: 1 ms (99.58%), Space: 44.1 MB (71.43%) - LeetHub

--- 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.java
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.java
@@ -1,32 +1,44 @@
+// class Solution {
+//     public int minimumSubarrayLength(int[] nums, int k) {
+//         int winsize=1;
+//         while(winsize<=nums.length){
+//             if(finder(nums,k,winsize)){
+//                 return winsize;
+//             }
+//             winsize++;
+
+//         }
+//         return -1;
+//     }
+//     public boolean finder(int nums[],int k,int winsize){
+//         int n=nums.length;
+//         for(int i=0;i<=n-winsize;i++){
+//             int kor=nums[i];
+//             for(int j=i+1;j<(i+winsize);j++){
+//                 kor|=nums[j];
+//             }
+//             if(kor>=k){
+//                 return true;
+//             }
+//         }
+//         return false;
+
+//     }
+// }
 class Solution {
     public int minimumSubarrayLength(int[] nums, int k) {
-        int winsize=1;
-        while(winsize<=nums.length){
-            if(finder(nums,k,winsize)){
-                return winsize;
-            }
-            winsize++;
-
-        }
-        return -1;
-    }
-    public boolean finder(int nums[],int k,int winsize){
-        int n=nums.length;
-        if(n==winsize){
-            int kor=0;
-            for(int i:nums) kor|=i;
-            return kor>=k;
-        }
-        for(int i=0;i<=n-winsize;i++){
-            int kor=nums[i];
-            for(int j=i+1;j<(i+winsize);j++){
-                kor|=nums[j];
-            }
-            if(kor>=k){
-                return true;
+        int n = nums.length;
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int orVal = 0;
+            for (int j = i; j < n; j++) {
+                orVal |= nums[j];
+                if (orVal >= k) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
             }
         }
-        return false;
-
+        return minLen == Integer.MAX_VALUE ? -1 : minLen;
     }
 }
