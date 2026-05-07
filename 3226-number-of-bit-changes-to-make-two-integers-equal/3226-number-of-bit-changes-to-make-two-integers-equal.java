46abfd3fc8d135893d9f4e24d001963693a8e4cb
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-05-08 04:11:34 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.7%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
@@ -1,16 +1,29 @@
+// class Solution {
+//     public int minChanges(int n, int k) {
+//         int ch=0;
+//         int times=0;
+//         while(times<=31){
+//             if((n&1) != (k&1)){
+//                 if((n&1)==1) ch++;
+//                 else return -1;
+//             }
+//             n>>=1;
+//             k>>=1;
+//             times++;
+//         }
+//         return ch;
+//     }
+// }
 class Solution {
     public int minChanges(int n, int k) {
-        int ch=0;
-        int times=0;
-        while(times<=31){
-            if((n&1) != (k&1)){
-                if((n&1)==1) ch++;
-                else return -1;
-            }
-            n>>=1;
-            k>>=1;
-            times++;
+        if((n&k)!=k) return -1;
+        int xor=n^k;
+        int count=0;
+        while(xor>0){
+            count+=xor&1;
+            xor>>=1;
         }
-        return ch;
+        return count;
+        
     }
 }
