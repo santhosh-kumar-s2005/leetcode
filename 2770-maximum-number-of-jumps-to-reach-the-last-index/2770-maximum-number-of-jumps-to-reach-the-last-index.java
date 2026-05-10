a6d3d8d546ac1fc984c3d210d248ceb344fe1445
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-05-11 01:53:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -6,7 +6,9 @@ class Solution {
         int maxjump=-1;
         for(int j=i-1;j>=0;j--){
             if(nums[j]-nums[i]>=-target && nums[j]-nums[i]<=target ){
-                maxjump=Math.max(maxjump,dp[j]);
+              //  maxjump=Math.max(maxjump,dp[j]);
+              maxjump=dp[j];
+              break;
             }
         }
         dp[i]=(maxjump==-1)?-1:maxjump+1;
