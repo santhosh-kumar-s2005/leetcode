ffe06fa2d07a76e18778a8b7e8d4a8095ef9c664
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-05-11 19:00:54 +0530

    Time: 0 ms (100%), Space: 47.3 MB (95.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
        static {
-        for(int i = 0; i < 10000; i++)
+        for(int i = 0; i < 100; i++)
             moveZeroes(new int[]{});
     }
     public static void moveZeroes(int[] nums) {
