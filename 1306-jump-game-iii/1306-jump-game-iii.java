814401a06c0801b91d484c9bcc405a695f490c1a
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-05-17 14:36:35 +0530

    Time: 1 ms (99.73%), Space: 51.2 MB (59.91%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int n;
+    public static boolean visited[];
+    public static boolean canReach(int[] nums, int start) {
+    n=nums.length;
+    if(nums[start]==0) return true;
+    if(n==1) return false;
+    visited=new boolean[n];
+    visited[start]=true;
+    return recjump(nums,start-nums[start])||recjump(nums,start+nums[start]);   
+    }
+    public static boolean recjump(int nums[],int jump_index){
+    if(jump_index<0 ||jump_index>=n||visited[jump_index]){
+        return false;
+    }
+    if(nums[jump_index]==0) return true;
+    visited[jump_index]=true;
+    return recjump(nums,jump_index-nums[jump_index])||recjump(nums,jump_index+nums[jump_index]);
+    }
+}
