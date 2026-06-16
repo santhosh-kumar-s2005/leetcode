6c1da2e9a490315fb16645d488a88954efc1f919
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-06-16 09:06:47 +0530

    Time: 20 ms (6.07%), Space: 53.3 MB (90.24%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -5,17 +5,13 @@ class Solution {
         }
 
         ArrayList<Integer> truster=new ArrayList<>();
-        HashMap<Integer,ArrayList<Integer>> map =new HashMap<>();
+        HashMap<Integer,Integer> map =new HashMap<>();
         for(int i[]:trust){
             truster.add(i[0]);
-            map.computeIfAbsent(i[1], k -> new ArrayList<>()).add(i[0]);
+            map.put(i[1],map.getOrDefault(i[1],0)+1);
         }
-      
-
-        
         for(int i:map.keySet()){
-            if(map.get(i).size()==n-1 && !truster.contains
-            (i)){
+            if(map.get(i)==n-1 && !truster.contains(i)){
                 return i;
             }
         }
