4a7642b54a552794799808b99837e2d96e36b681
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-06-17 14:35:41 +0530

    Time: 644 ms (87.55%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement belo
-select s.user_id,round(
-    if(sum(action='confirmed')/count(action) is null,0,sum(action='confirmed')/count(action)),2) as confirmation_rate
+select s.user_id,round(         case
+                                 when sum(action='confirmed')/count(action) is null then 0
+                                else sum(action='confirmed')/count(action)
+                                end 
+                                ,2) as confirmation_rate
 from signups s
 left join confirmations c
 on s.user_id=c.user_id
