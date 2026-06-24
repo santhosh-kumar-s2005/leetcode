e57a6e84961f82e4c4b511dfeeb3f6fe72cec35d
Santhosh kumar .S <23it1218@stjosephstechnology.ac.in>
2026-06-24 11:19:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (37.91%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public int integerBreak(int n) {
+//         int dp[]=new int[n+1];
+//         dp[1]=0;
+//         dp[2]=1;
+//         for(int i=3;i<=n;i++){
+//             for(int j=i-1;j>=(i/2);j--){
+//                 int val=(j>dp[j])?j:dp[j];
+//                 val*=i-j>dp[i-j]?i-j:dp[i-j];
+//                 dp[i]=Math.max(dp[i],val);
+//             }
+//         }
+//         return dp[n];
+        
+//     }
+// }
 class Solution {
     public int integerBreak(int n) {
-        int dp[]=new int[n+1];
-        dp[1]=0;
-        dp[2]=1;
-        for(int i=3;i<=n;i++){
-            for(int j=i-1;j>=(i/2);j--){
-                int val=(j>dp[j])?j:dp[j];
-                val*=i-j>dp[i-j]?i-j:dp[i-j];
-                dp[i]=Math.max(dp[i],val);
-            }
+       if (n == 2) {
+            return 1;
         }
-        return dp[n];
-        
+        if (n == 3) {
+            return 2;
+        }
+
+        // Try to divide n into as many threes as possible
+        int threes = n / 3;
+        int remainder = n % 3;
+
+        if (remainder == 1) {
+            threes -= 1; // remove 3 * 1
+            remainder = 4; // create 2 * 2
+        } else if (remainder == 0) {
+            remainder = 1; // when remainder is 0, set 1 which doesn't affect your answer.
+        }
+
+        return (int) (Math.pow(3, threes) * remainder);        
     }
 }
